e80f4e21cb5b47e973c1d379a371fbf7f09d7cf4
Hoshi03 <ichanho114@gmail.com>
2025-02-07 16:49:30 +0900

    [fix] 페이징 버튼 css 수정
    페이징 버튼 css 수정

--- src/main/java/com/tastehill/myweb/common/PagingUtil.java
+++ src/main/java/com/tastehill/myweb/common/PagingUtil.java
@@ -17,71 +17,64 @@ public class PagingUtil {
 	 * @param blockPage  	: 한화면에 보여질 블럭 수
 	 **/
 	public PagingUtil(String url, int currentPage, int totRecord, int blockCount, int blockPage) {
-		// (1) 최대 페이지 수 구하기 (Math.ceil 올림)
-		maxPage = (int) Math.ceil((double) totRecord / blockCount);
-		if (maxPage == 0) {
-			maxPage = 1;
-		}
+	    maxPage = (int) Math.ceil((double) totRecord / blockCount);
+	    if (maxPage == 0) {
+	        maxPage = 1;
+	    }
 
-		// 예외처리 : 현재 페이지가 전체 페이지 수보다 크면 전체 페이지 수로 설정
-		if (currentPage > maxPage) {
-			currentPage = maxPage;
-		}
+	    if (currentPage > maxPage) {
+	        currentPage = maxPage;
+	    }
 
-		// (2) 현재 페이지의 처음과  끝 글번호 가져오기
-		startSeq = (currentPage - 1) * blockCount + 1;
-		endSeq = currentPage * blockCount;
+	    startSeq = (currentPage - 1) * blockCount + 1;
+	    endSeq = currentPage * blockCount;
 
-		// (3) 시작페이지와 끝페이지 값 구하기  
-		if ( currentPage % blockPage == 0 ) {
-			startPage = currentPage - ( blockPage - 1 );
-		} else {
-			startPage = (int)(currentPage / blockPage) * blockPage +1;
-		}
-		endPage = startPage + blockPage - 1;
+	    if (currentPage % blockPage == 0) {
+	        startPage = currentPage - (blockPage - 1);
+	    } else {
+	        startPage = (int)(currentPage / blockPage) * blockPage + 1;
+	    }
+	    endPage = startPage + blockPage - 1;
 
-		// 예외처리 : 마지막페이지가 전체페이지수보다 크면 전체페이지 수로 설정하기
-		if (endPage > maxPage) {
-			endPage = maxPage;
-		}
+	    if (endPage > maxPage) {
+	        endPage = maxPage;
+	    }
 
-		//################## HTML 만들기 ###################
-		// [이전] HTML
-		pagingHtml = new StringBuffer();
-		if (currentPage > blockPage) {
-			pagingHtml.append("<a href='" + "http://localhost:8089" + url +"&currentPage="  + (startPage - 1) + "'>");
-			pagingHtml.append("이전");
-			pagingHtml.append("</a>");
-		}
+	    //################## HTML 만들기 ###################
+	    pagingHtml = new StringBuffer();
+	    pagingHtml.append("<div class='pagination'>");
 
-		pagingHtml.append(" | ");
-		// |1|2|3|4|5|  HTML (현재 페이지는 빨간색으로 강조하고 링크 제거)
-		for (int i = startPage; i <= endPage; i++) {
-			if (i > maxPage) {
-				break;
-			}
-			if (i == currentPage) {
-				pagingHtml.append(" <b><font color='red'>");
-				pagingHtml.append(i);
-				pagingHtml.append("</font></b>");
-			} else {
-				pagingHtml.append(" <a href='" + url +"&currentPage=");
-				pagingHtml.append(i);
-				pagingHtml.append("'>");
-				pagingHtml.append(i);
-				pagingHtml.append("</a>");
-			}
+	    // [이전] 버튼
+	    if (currentPage > blockPage) {
+	        pagingHtml.append("<a href='" + url + "&currentPage=" + (startPage - 1) + "' class='prev'>");
+	        pagingHtml.append("이전");
+	        pagingHtml.append("</a>");
+	    }
 
-			pagingHtml.append(" ");
-		}
-		pagingHtml.append("  |  ");
+	    // |1|2|3|4|5| (현재 페이지는 active 클래스 적용)
+	    for (int i = startPage; i <= endPage; i++) {
+	        if (i > maxPage) {
+	            break;
+	        }
+	        if (i == currentPage) {
+	            pagingHtml.append(" <a class='active'>");
+	            pagingHtml.append(i);
+	            pagingHtml.append("</a>");
+	        } else {
+	            pagingHtml.append(" <a href='" + url + "&currentPage=" + i + "'>");
+	            pagingHtml.append(i);
+	            pagingHtml.append("</a>");
+	        }
+	    }
 
-		// [다음] HTML
-		if (maxPage - startPage >= blockPage) {
-			pagingHtml.append("<a href='" + url +"&currentPage="  + (endPage + 1) + "'>");
-			pagingHtml.append("다음");
-			pagingHtml.append("</a>");
-		}
+	    // [다음] 버튼
+	    if (maxPage - startPage >= blockPage) {
+	        pagingHtml.append("<a href='" + url + "&currentPage=" + (endPage + 1) + "' class='next'>");
+	        pagingHtml.append("다음");
+	        pagingHtml.append("</a>");
+	    }
+
+	    pagingHtml.append("</div>"); // `.pagination` 닫기
 	}
 
 	
